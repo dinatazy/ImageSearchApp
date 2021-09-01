2fa9a09460904a8472a2901323769be046776802
Dina <dina.altazy@gmail.com>
2021-09-01 09:34:25 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { ImageSearch } from './screens/image-search/ImageSearch'
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="ImageSearch">
+        <Stack.Screen name="ImageSearch" component={ImageSearch} options={{ title: 'Search Image' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+export default App;
---
+++ android/app/src/main/java/com/imagesearchapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.imagesearchapp;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ImageSearchApp";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
+}
